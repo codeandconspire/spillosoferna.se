f1a7fdb31cba907be048260b5adb482b5784213f
Carl Törnqvist <carl@tornqv.ist>
2019-10-23 15:15:15 +0200

    Add details fallback

--- components/more/index.js
+++ components/more/index.js
@@ -4,13 +4,17 @@ var { text } = require('../base')
 module.exports = more
 
 function more (preamble, body) {
+  var id = 'more-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
   return html`
     <details class="More">
+      <input type="checkbox" id="${id}" class="More-checkbox" hidden>
       <summary class="More-summary">
         <div class="More-preamble">${preamble}</div>
-        <span class="More-toggle">+ ${text`Läs mer`}</span>
+        <label for="${id}" class="More-toggle">+ ${text`Läs mer`}</label>
       </summary>
-      ${body}
+      <div class="More-body">
+        ${body}
+      </div>
     </details>
   `
 }
